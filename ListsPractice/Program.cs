a852d83f2ebb7fbf6eed4ce39273e763343b8074
Sam Jacobson <samuel.c.jacobson@gmail.com>
2017-07-12 13:24:12 -0400

    edits

--- ListsPractice/Program.cs
+++ ListsPractice/Program.cs
@@ -65,6 +65,54 @@ namespace ListsPractice
             {
                 Console.WriteLine("I like fish too!");
             }
-    }
+
+            //Create a list with the following numbers: 1 23 9 77 922 6 32 63 14 5
+            //Use the contains method with the following values: 23 77 15
+            //Remove these elements: 23 77 32 6 (.Remove method)
+            //Use contains again on these values: 23 77 15
+            List<int> numbers = new List<int> { 1, 23, 9, 77, 922, 6, 32, 63, 14, 5 };
+            Console.WriteLine(numbers.Contains(23));
+            Console.WriteLine(numbers.Contains(77));
+            Console.WriteLine(numbers.Contains(15));
+            numbers.Remove(23);
+            numbers.Remove(77);
+            numbers.Remove(32);
+            numbers.Remove(6);
+            Console.WriteLine(numbers.Contains(23));
+            Console.WriteLine(numbers.Contains(77));
+            Console.WriteLine(numbers.Contains(15));
+
+            //Ask the user for a movie, if not in list add and inform user it has been ordered
+            //If the movie is in list inform that it is on the way
+            //If user enters quit the program should exit
+            //The user should be able to add as many movies as they want
+            //When the user quits show them all of the movies being ordered
+            List<string> movies = new List<string> { "Office Space", "The Big Lebowski", "Pulp Fiction", "Amadeus", "Rushmore" };
+            string movieEntry;
+            do
+            {
+                Console.WriteLine("Please enter the movie you would like to order.");
+                Console.WriteLine("You may enter \"quit\" when finished.");
+                movieEntry = Console.ReadLine();
+                if (movieEntry.ToLower() == "quit")
+                {
+                    Console.WriteLine("Thank you for your order! You will be receiving the following:");
+                    foreach(string movie in movies)
+                    {
+                        Console.WriteLine(movie);
+                    }
+                }
+                else if (movies.Contains(movieEntry))
+                {
+                    Console.WriteLine(movieEntry + " is in stock and on the way!");
+                }
+                else
+                {
+                    Console.WriteLine(movieEntry + " has been ordered and will available in 3-5 days.");
+                    movies.Add(movieEntry);
+                }
+            }
+            while (movieEntry.ToLower() != "quit");
+        }
     }
 }
